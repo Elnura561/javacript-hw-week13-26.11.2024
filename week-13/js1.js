8b1fd4bac5f8bdb332bfc35541b533de780feffe
Elnura561 <elnuramametova6@gmail.com>
2024-11-27 08:02:25 +0500

    сист

---
+++ week-13/js1.js
@@ -0,0 +1,90 @@
+// let jidekter=['alma','shie','banan']
+// console.log(jidekter[1],jidekter[2]);
+
+// jidekter[1]='mango'
+// console.log(jidekter);
+
+// jidekter[3]='almurt'
+// console.log(jidekter);
+
+// let jemister=[]
+// console.log(jemister);
+
+// let jemis1=prompt('jemis engiz')
+// jemister[0]=jemis1
+// console.log(jemister);
+
+// // Value type----бир-биримен байланыспаган озгермейди 
+// let a=5
+// let b=a;
+// a=7;
+// console.log('a:'+a); //a=7 
+// console.log('b:'+b); //b=5
+
+
+
+//Reference---бир-биримен байланыскан =class room
+// let arr1=[4,6,8]
+// let arr2=arr1;
+// arr2[2]=22;
+// arr1[0]=15;
+// console.log(`arr2:${arr2}`); // 15,6,22
+// console.log(`arr1:${arr1}`); //15,6,22
+
+// тапсырма -1---
+//let fruits = ['Alma','banan','shie'];
+// fruits[3]='apelsin'
+// console.log(fruits);
+
+// //тапсырма-2--
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+// console.log(fruits[3]);
+
+
+// //тапсырма-3---
+// // console.log(fruits.length);//узындыгы тартып алу
+// let fruits = ['Alma','banan','shie'];
+// for(let i=0; i<fruits.length;i++){
+// console.log(fruits[i]);
+    
+// }
+
+//
+
+//тапсырма-4----
+    // let sany =+prompt('qansha fruits kerek');
+  
+    // let fruits=[]
+        
+    // for(let i=0; sany>i;i++){
+    //      let b=prompt((i+1)+'-index aty');
+    //     fruits[i]=b
+    // }
+    // console.log(fruits);
+    
+//hw-26.11.2024--
+let san=['Ronaldu','Zidan','Neimar','Venzema'];
+san [1] ='Messi';
+san [4]='Remma'
+san[5]='ERni'
+console.log(san);
+ if(san.length>5){
+        console.log('qop');
+    }else  {       
+        console.log('az');
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
