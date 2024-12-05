c33bf1cc32d4e74296a7ea15aac31cf669ba8d9e
Elnura561 <elnuramametova6@gmail.com>
2024-12-05 08:30:00 +0500

    hw-5.12.2021

--- week-14/js.js
+++ week-14/js.js
@@ -187,58 +187,58 @@
                             
 // тапсырма--1---
 
-let course = {
-  course_name: "Amjilt Cyber School",
-  mugalimder: [
-    {
-      name:"Galymbek",
-      age:30,
-      hobbies:["Coding","SanzhardinQaninIwu","Gaming","Sleeping"]
-    },
-    {
-      name:"Dilnaz",
-      age:18,
-      hobbies:["Speaking","Swimming","Running","Englishing"]
-    },
-    {
-      name:"Islam",
-      age:26,
-      hobbies:["Islaming","Coding","Muzik"]
-    },
-    {
-      name:"Ainur",
-      age:17,
-      hobbies:["Checking","Googling","Kuratorling","Bagging"]
-    }
-  ],
-  oqushilar: [
-    {
-      name:"Mahaoya",
-      age:30,
-      hobbies:["Coding","Make","Chining"]
-    },
-    {
-      name:"Sanzhar",
-      age:29,
-      hobbies:["Make-Sake","More-questioning","Sleeping","Sulpaking"]
-    },
-    {
-      name:"Islam",
-      age:16,
-      hobbies:["Islaming","Footbik","Slowling","Thinking"]
-    },
-    {
-      name:"Beibarys",
-      age:22,
-      hobbies:["Proguling","OsyGanaing","Bibaing"]
-    }
-  ]
-}
+// let course = {
+//   course_name: "Amjilt Cyber School",
+//   mugalimder: [
+//     {
+//       name:"Galymbek",
+//       age:30,
+//       hobbies:["Coding","SanzhardinQaninIwu","Gaming","Sleeping"]
+//     },
+//     {
+//       name:"Dilnaz",
+//       age:18,
+//       hobbies:["Speaking","Swimming","Running","Englishing"]
+//     },
+//     {
+//       name:"Islam",
+//       age:26,
+//       hobbies:["Islaming","Coding","Muzik"]
+//     },
+//     {
+//       name:"Ainur",
+//       age:17,
+//       hobbies:["Checking","Googling","Kuratorling","Bagging"]
+//     }
+//   ],
+//   oqushilar: [
+//     {
+//       name:"Mahaoya",
+//       age:30,
+//       hobbies:["Coding","Make","Chining"]
+//     },
+//     {
+//       name:"Sanzhar",
+//       age:29,
+//       hobbies:["Make-Sake","More-questioning","Sleeping","Sulpaking"]
+//     },
+//     {
+//       name:"Islam",
+//       age:16,
+//       hobbies:["Islaming","Footbik","Slowling","Thinking"]
+//     },
+//     {
+//       name:"Beibarys",
+//       age:22,
+//       hobbies:["Proguling","OsyGanaing","Bibaing"]
+//     }
+//   ]
+// }
 
-console.log(course.oqushilar[0])
-console.log(course.oqushilar[1])
+// console.log(course.oqushilar[0])
+// console.log(course.oqushilar[1])
 
-// course.oqushilar[2].age = 17
+// // course.oqushilar[2].age = 17
 
 // console.log(course.oqushilar);
 
@@ -297,19 +297,150 @@ console.log(course.oqushilar[1])
 // }
 // enjas(classes);
     //HW-4.12.2024---
-let artists =[{
-  name:'Michael Jakson',songs:['Smoot Criminal','Billie it','Earth Song']},
-  {name:'Billie Eilish',songs:['Bad Guy','Lovely','Chihiro']},
-  {name:'Kairat Nurtas',songs:['Auyrmaydy Zhurek','Ol sen emes']}
-]
-console.log(artists);
+// let artists =[{
+//   name:'Michael Jakson',songs:['Smoot Criminal','Billie it','Earth Song']},
+//   {name:'Billie Eilish',songs:['Bad Guy','Lovely','Chihiro']},
+//   {name:'Kairat Nurtas',songs:['Auyrmaydy Zhurek','Ol sen emes']}
+// ]
+// console.log(artists);
 
-artists[2].songs='Baika'
-console.log(artists);
+// artists[2].songs='Baika'
+// console.log(artists);
 
-console.log(artists[0].name);
-console.log(artists[0].songs[2]);
+// console.log(artists[0].name);
+// console.log(artists[0].songs[2]);
 
-console.log(artists[1].songs.filter(par=>par.length<=6));
+// console.log(artists[1].songs.filter(par=>par.length<=6));
+
+//4,12,2024---class-
+
+// let massiv=[46,25,15,1]
+
+// let[san1,san2,san3,san4]=massiv
+// console.log(massiv[0]); //--46 chigarady
+// console.log(san1); //--46 chigarady
+
+// let object={
+//   name:'Asan',
+//   age:19,
+//   hobby:'play game'
+// }
+// let {age,hobby,name}=object //--suray kerek osilai
+// console.log(object.name); //--Asan
+
+// console.log(name); //--Asan
+//     //spread(...)=кенейту
+// let arr=[1,2,3,4,5]
+// let arr1=[...arr,6,7,8,9,10,...arr] //1.2.3.4.5.6.7..9.10 chigarady kosip jiberu
+// console.log(arr1);
+
+// let obj1 ={name:'Galimbek',age:30}
+// let obj2 ={...obj1,hobby:'Codding'}
+// console.log(obj2);
+
+//    //Rest(...)=жинактау
+
+// //  let array=[5,6,74,25,11]
+// //  let[a,b,...rest]=array
+// //  console.log(rest);
+   
+// let {name,...qalgany}={name:'Asan',age:19,hobby:'play game'}
+// console.log(name);
+// console.log(qalgany.age);
+
+// let person={
+//   name: prompt('Atyndy jaz'),
+//   age: +prompt('jazindy jaz'),
+//   hobby: prompt('Hobby jaz'),
+//   allInfo:function(){
+//     return`senin atyn: ${this.name}\nSenin jasin: ${this.age}\nSenin hobbyn:${this.hobby}`
+//   }
+// }
+// alert(person.allInfo())
+// let{name,age,hobby}=person
+// if(age<0){
+//   alert('SEN aly pland joksin')
+// }
+// else if(age>18 && age<100){
+
+//   alert('sen ereseksin')
+// }else if(age>100){
+//   alert('dinozabor')
+
+// }else{
+//   alert('sen shigolsin')
+// }
+// тапсырма--1--
+
+// const book={title:'Abai joly',author:'Muhtar Auezov',yaer:1942};
+// const{title,author,yaer}=book
+// console.log(book.author);
+// console.log(book.title);
+// console.log(book.yaer);
+
+//   //тапсырма--2---
+//   const fruits=['alma','almurt','chie']
+//   const [a,...rest]=fruits
+//   console.log(rest);
+
+//   //тапсырма--3--
+//   const arr1=[1,2,3];
+//   const arr2=[4,5,6];
+//   const ar=[...arr1,...arr2] //массивтерди бириктиру
+//   console.log(ar);
+  
+// //тапсырма--4--
+// const user ={name:'Ali',age:25};
+// const us={...user,city:'Almaty'}
+// console.log(us);
+
+//   //тапсырма-5---
+
+//   let adam=[{
+//     name:'Magauia',
+//     age:30,
+//     city:'China'
+//   },
+//   {
+//     name:'Berik',
+//     age:25,
+//     city:'Almaty'
+//   },
+
+//   {
+//     name:'Elnura',
+//     age:37,
+//     city:'Almaty'
+//   },
+ 
+// ]
+let oqu ={
+  oquchi:{
+  name:'Aslan',
+  math:85,
+  city:'Almaty',
+  mektep:'35 mektep'
+},
+oquchilar:{
+  name:'Sungat',
+  math:100,
+  city:'Astana',
+  county:'Kazakhstan'
+
+}
+}
+let{name,math,mektep,city,county}=oqu.oquchilar;
+console.log(`Aty:${name}`);
+console.log(`Baga:${math}`);
+console.log(`Qala:${city}`);
+console.log(`Memeleket:${county}`);
+
+
+let classA=['asan','Arman','Nuraiym'];
+let classB=['Sungat','Kanat','Islam'];
+let clases=['Direktor',...classA,...classB,'Mugelim'];
+
+console.log('Tolik tizim:',clases);
+
 
 
