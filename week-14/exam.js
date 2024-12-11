81b8d13c45c8be12944ae1762c93227ef0df95fc
Elnura561 <elnuramametova6@gmail.com>
2024-12-11 08:12:39 +0500

    hw-11.12.2024 DOM

--- week-14/exam.js
+++ week-14/exam.js
@@ -1,55 +1,108 @@
-let eShop={
-    name:'Elnura',
-    phoneNumber:8707252,
-    product:[{
-        productname:'Nutella',
-        category:'Nan',
-        price:500,
-        stock:300
-    },
-    {
-        productname:'Assam tea',
-        category:'tea',
-        price:200,
-        stock:350
-    },
-    {
-        productname:'Aral tuz',
-        category:'tuz',
-        price:700,
-        stock:700
-    },
-    {
-        productname:'Banan',
-        category:'jemis',
-        price:1500,
-        stock:100
-    },
-    {
-        productname:'Norka',
-        category:'closes',
-        price:300,
-        stock:150
+let eShop = {
+    name:"Make-Sake",
+    phoneNumber: "87076556565",
+    products:[
+        {
+            productName:"Cola",
+            category:"Susyn",
+            price: 500,
+            stock:15  
+        },
+        {
+            productName:"Kozildirik",
+            category:"Aksessuar",
+            price: 4500,
+            stock:7  
+        },
+        {
+            productName:"Marker",
+            category:"Kanceliarya",
+            price: 350,
+            stock:55  
+        },
+        {
+            productName:"VODA",
+            category:"Susyn",
+            price: 350,
+            stock:32  
+        },
+        {
+            productName:"Iphone 15 pro",
+            category:"Electronic",
+            price: 750000,
+            stock:10 
+        }
+    ],
+    // changeShopName: function(){
+    //     let janaAty = prompt("Jana atau engiz")
+    //     if(janaAty == ""  janaAty == null  janaAty == undefined){
+    //         this.name = this.name
+    //     }else{
+    //         this.name = janaAty // "Make-Sake" => "BestShop"
+    //     }
+    // },
+    // changePhoneNumber: function(){
+    //     let janaNomer = prompt("Jana nomer engiz")
+    //     if(janaNomer == ""  janaNomer == null  janaNomer == undefined){
+    //         this.phoneNumber = this.phoneNumber
+    //     }else{
+    //         this.phoneNumber = janaNomer // "87006556565" => "..."
+    //     }
+    // },
+    // listProductNames: function(){
+    //     let onimder = []
+    //     for(let element of this.products){
+    //         onimder.push(element.productName)
+    //     }
+    //     alert(onimder.join('\n'))
+    // },
+    // addProduct:function(){
+    //     let onimAty = prompt("Jana onim atyn jaz")
+    //     let onimSanaty = prompt("Jana onim sanatyn jaz")
+    //     let onimBaga = +prompt("Jana onim bagasyn jaz")
+    //     let onimQalgany = +prompt("Jana onim qaldygyn jaz")
+
+    //     let janaOnim = {}
+
+    //     if(onimAty && onimSanaty && onimBaga && onimQalgany ){
+    //         janaOnim = {productName: onimAty, category: onimSanaty, price: onimBaga, stock: onimQalgany}
+    //         if(janaOnim){
+    //             this.products.push(janaOnim)
+    //         }
+    //     }else{
+    //         alert("Onim bir sipattamasyn engizbediniz!")
+    //     }
+    // },
+    applyDiscountToCategory:function(){
+        let jenildikCategory = prompt("Qandai sanatqa jenildik jasaisyz?").toLowerCase() //SUSYN ->susyn
+
+        if(jenildikCategory != null && jenildikCategory != "" && jenildikCategory != undefined){
+            let jenildik = +prompt("Qansha paiz jenildik jasaisyz?(0-100)")
+            for(let i=0;i<this.products.length;i++){
+                if(this.products[i].category.toLowerCase() == jenildikCategory){ 
+                    this.products[i].price = this.products[i].price - ((this.products[i].price * jenildik)/100)
+                }
+            }
+        }   
     }
-],
 }
+console.log("Функция шақырылмай тұрып", eShop);
 
-console.log(eShop);
-// function changeShopName(){
-//   //  eShop.productname=prompt('magazin atayh jaz')}
-// console.log(changeShopName());
+// Tapsyrma 2
+// eShop.changeShopName()
+// console.log(eShop);
 
-// function changephoneNumber(){
+// Tapsyrma 3
+// eShop.changePhoneNumber()
+// console.log(eShop);
 
-// // }
-// function listProductNames(){
-//     return`product aty:${(this.category)}`
-// }
-// alert(listProductNames())
+// Tapsyrma 4
+// eShop.listProductNames()
 
-// function addProduct(){
-    
-//     this.product.push()
-// }
-// addProduct()
+// Tapsyrma 5
+// eShop.addProduct()
+// console.log(eShop);
 
+// Tapsyrma 6
+// eShop.applyDiscountToCategory()
+// console.log(eShop);
---
+++ week-15/js.js
@@ -0,0 +1,49 @@
+// let taqyrip=document.getElementById("title")
+// console.log(taqyrip.textContent);
+
+// let paragrab =document.getElementsByClassName("text")
+// console.log(paragrab);
+
+// console.log(paragrab[0].textContent);
+// console.log(paragrab[1].textContent);
+
+// //циклмен осылай кыска турде сурап алсак болды 1тур
+// for(let i=0;i<paragrab.length;i++){
+//     console.log(paragrab[i].textContent);
+    
+// }
+// //2тури осылай тоже алсак болады
+// for(let element of paragrab){
+//     console.log(element.textContent);
+    
+// }
+// //1гана алады
+// //let title= document.querySelector(".text")
+// //console.log(title.textContent);
+
+// //барин алып береди
+// let title=document.querySelectorAll(".text")
+// console.log(title[0].textContent);
+
+
+// let qorap =document.getElementById('qorap');
+// qorap.innerHTML="<h1 class='hello'>Qorapka text salindy</h1>";
+// let title=document.getElementById('contener')
+// title.textContent='new topic'
+// title.innerHTML=""
+// title.innerHTML='<p>HEEllo</p>'
+
+          //HW-11.12.2024years----
+
+let id=document.querySelector('#contener')
+contener.textContent='Homework Day1 DOM' 
+
+let text=document.getElementById('text')
+text.textContent='Ozinin tolyk aty-joniniz';
+
+let hobby =document.getElementsByClassName('my')
+hobby[0].textContent='Hobby1';
+hobby[1].textContent='Hobby2';
+hobby[2].textContent='Hobby3'
+
+
